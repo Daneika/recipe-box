7d2ca15b8cbd7a661d2e0129308291f3755b1fcf
meg <megan.daneika@gmail.com>
2020-07-03 13:16:56 +0100

    Add Tooltip to recipe cards

--- src/components/recipes/Recipes.js
+++ src/components/recipes/Recipes.js
@@ -5,6 +5,8 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
 
 export default function Recipes(props) {
   return (
@@ -14,17 +16,25 @@ export default function Recipes(props) {
           {props.recipes.map((recipe) => {
             return (
               <Col key={recipe.title}>
-                <Card style={{ width: "18rem", height:"22rem"}}>
+                <Card style={{ width: "18rem", height: "22rem" }}>
                   <Card.Img
                     src={`https://spoonacular.com/recipeImages/${recipe.id}-312x150.jpg`}
                   />
                   <Card.Body>
-                    <Card.Title>{recipe.title.length < 20 ? `${recipe.title}` : `${recipe.title.substring(0, 25)}...`}</Card.Title>
+                    <OverlayTrigger
+                      key={recipe.title}
+                      placement="right"
+                      overlay={<Tooltip>{recipe.title}</Tooltip>}
+                    >
+                      <Card.Title>
+                        {recipe.title.length < 20
+                          ? `${recipe.title}`
+                          : `${recipe.title.substring(0, 25)}...`}
+                      </Card.Title>
+                    </OverlayTrigger>
                     <Card.Text>
                       {`Duration: ${recipe.readyInMinutes}mins`}
-                      <div>
-                      {`Serves: ${recipe.servings}`}
-                      </div>
+                      <div>{`Serves: ${recipe.servings}`}</div>
                     </Card.Text>
                     <Button variant="success">View Recipe</Button>
                   </Card.Body>
